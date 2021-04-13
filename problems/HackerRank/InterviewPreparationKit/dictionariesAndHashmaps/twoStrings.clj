07566ea6fd1d67f0f8ce0617d67651289f782c13
Gustavo Oliveira <gustavo.de.of@gmail.com>
2021-04-12 23:33:09 -0300

    updating twoStrings problem

--- problems/HackerRank/InterviewPreparationKit/dictionariesAndHashmaps/twoStrings.clj
+++ problems/HackerRank/InterviewPreparationKit/dictionariesAndHashmaps/twoStrings.clj
@@ -1,13 +1,12 @@
-; https://www.hackerrank.com/challenges/two-strings
+; https://www.hackerrank.com/challenges/two-strings      
 (defn twoStrings [s1 s2]
   (let [s2Size (count s2)
         s1Map (frequencies s1)]
     (loop [index 0]
-      (if (s1Map (get s2 index))
-        "YES"
-        (if (= index s2Size)
-          "NO"
-          (recur (inc index)))))))
+      (cond
+        (s1Map (get s2 index)) "YES"
+        (= index s2Size) "NO"
+        :else (recur (inc index))))))
 
 (twoStrings "and" "art")
 (twoStrings "be" "cat")
