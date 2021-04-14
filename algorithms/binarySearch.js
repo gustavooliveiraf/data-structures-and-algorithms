38d21eb4a4af50ec41a95961698872c7531e86fe
Gustavo Oliveira <gustavo.de.of@gmail.com>
2021-04-14 02:02:26 -0300

    updating binarySearch problem

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -13,5 +13,35 @@ function binarySearch(arr, x) {
   return -1;
 }
 
-const arr = [1,2,3,4];
-console.log(binarySearch(arr, 1));
+function binarySearchLowerBound(arr, x) {
+  let l = 0;
+  let r = arr.length;
+
+  while (l < r) {
+    const mid = Math.floor((l + r) / 2);
+
+    if (x <= arr[mid]) r = mid;
+    else l = mid + 1;
+  }
+
+  return l;
+}
+
+function binarySearchUpperBound(arr, x) {
+  let l = 0;
+  let r = arr.length;
+
+  while (l < r) {
+    const mid = Math.floor((l + r) / 2);
+
+    if (x >= arr[mid]) l = mid + 1;
+    else r = mid;
+  }
+
+  return l;
+}
+
+const arr = [10, 10, 10, 20, 20, 20, 30, 30];
+console.log(binarySearch(arr, 20));
+console.log(binarySearchLowerBound(arr, 20));
+console.log(binarySearchUpperBound(arr, 20));
