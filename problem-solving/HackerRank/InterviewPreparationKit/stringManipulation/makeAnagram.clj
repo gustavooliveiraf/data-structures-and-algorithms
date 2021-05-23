66f4fbd01b2a4957ad823f0c5a35017c8a2cfc94
Gustavo Oliveira <gustavo.de.of@gmail.com>
2021-05-22 21:28:46 -0300

    updating makeAnagram

--- problem-solving/HackerRank/InterviewPreparationKit/stringManipulation/makeAnagram.clj
+++ problem-solving/HackerRank/InterviewPreparationKit/stringManipulation/makeAnagram.clj
@@ -3,10 +3,10 @@
   (let [a-map (frequencies a)
         b-map (frequencies b)]
     (+ (reduce (fn [accu [k v]] (+ accu (Math/abs (- v (b-map k 0)))))
-             0
-             a-map)
+               0
+               a-map)
        (reduce (fn [accu [k v]] (+ accu (if (contains? a-map k) 0 v)))
-                0
-                b-map))))
+               0
+               b-map))))
 
 (makeAnagram "cde" "abc")
