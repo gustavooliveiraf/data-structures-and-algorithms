52e4431a0fe2e82b7f4838da760ef6aba7d394a8
Gustavo Oliveira <gustavo.de.of@gmail.com>
2021-05-24 23:03:54 -0300

    adding lowestCommonAncestor problem

---
+++ problem-solving/HackerRank/InterviewPreparationKit/trees/lowestCommonAncestor.cpp
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/binary-search-tree-lowest-common-ancestor
+
+Node *lca(Node *root, int v1,int v2) {
+  if (root == NULL)
+    return root;
+
+  if (root->data < v1 && root->data < v2)
+    return lca(root->right, v1, v2);
+
+  if (root->data > v1 && root->data > v2)
+    return lca(root->left, v1, v2);
+
+    return root;
+}
