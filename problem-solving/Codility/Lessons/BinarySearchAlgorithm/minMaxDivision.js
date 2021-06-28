a79a0f23cd3f7238c48daf1e7b1d01813f3051b1
Gustavo Oliveira <gustavo.de.of@gmail.com>
2021-06-27 23:11:52 -0300

    updating minMaxDivision problem Codility

--- problem-solving/Codility/Lessons/BinarySearchAlgorithm/minMaxDivision.js
+++ problem-solving/Codility/Lessons/BinarySearchAlgorithm/minMaxDivision.js
@@ -1,4 +1,5 @@
 // https://app.codility.com/programmers/lessons/14-binary_search_algorithm/min_max_division/
+// Score 100%: https://app.codility.com/demo/results/trainingXWYENR-FJ3/
 function isNumberBlocksLessThanK(arr, middle, n, k) {
   let count = 1;
   let accumulator = 0;
@@ -17,13 +18,13 @@ function isNumberBlocksLessThanK(arr, middle, n, k) {
   return true;
 }
 
-function solution(k, m, arr) {
+function solution(k, _, arr) {
   let start = arr.reduce((a, b) => Math.max(a, b));
   let end   = arr.reduce((a, b) => a + b);
   let middle, response;
 
   while (start <= end) {
-    middle = parseInt((start + end) / 2);
+    middle = (start + end) >> 1;
 
     if (isNumberBlocksLessThanK(arr, middle, arr.length, k)) {
       response = middle;
@@ -36,6 +37,5 @@ function solution(k, m, arr) {
   return response;
 }
 
-const arr = [2, 1, 5, 1, 2, 2, 2];
-const response = solution(3, 5, arr);
-console.log(response);
+const arr = [2, 1, 5, 1, 2, 2, 2], k = 3, m = 5;
+console.log(solution(k, m, arr));
