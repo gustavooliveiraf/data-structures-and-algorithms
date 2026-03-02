cb9b402303796256de669cbf9e3e9b5e25d264e3
Gustavo Oliveira <gustavo.oliveira@180s.com.br>
2026-03-02 18:50:18 -0300

    adding hasCycle solution

---
+++ problem-solving/Leetcode/ProblemSolving/linkedList/hasCycle.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/linked-list-cycle
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  let slowPointer = head;
+  let fastPointer = head;
+
+  while (fastPointer !== null && fastPointer.next !== null) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+
+    if (fastPointer === slowPointer) {
+      return true;
+    }
+  }
+
+  return false;
+};
